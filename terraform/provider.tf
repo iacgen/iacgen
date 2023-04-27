4225f229606256fb086ef1d1b48ec901f46c21b4
iacgen <iacgen.appcd@gmail.com>
2023-04-27 20:39:01 +0530

    update provider, network template

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 2.0"
   region  = var.region
 }
 
